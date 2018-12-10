542d6b4bab1ed6e9374798151b303e7316d77d9d
omelt <mage_of_code@outlook.com>
2018-12-10 11:39:22 +0800

    add DeadLoopTest

---
+++ src/com/company/DeadLoopTest.java
@@ -0,0 +1,30 @@
+package com.company;
+
+class DeadLoopClass {
+    static {
+        if (true) { //不加这个编译出错 Initializer does not complete normally  java: 初始化程序必须能够正常完成
+            System.out.println(Thread.currentThread() + "init DeadLoopClass");
+            while (true) {
+            }
+        }
+    }
+
+}
+public class DeadLoopTest {
+
+
+    public static void main(String[] args) {
+        Runnable script = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread() + " start");
+                DeadLoopClass deadLoopClass = new DeadLoopClass();
+                System.out.println(Thread.currentThread() + " run over");
+            }
+        };
+        Thread thread1 = new Thread(script);
+        Thread thread2 = new Thread(script);
+        thread1.start();
+        thread2.start();
+    }
+}
--- src/com/company/StaticBlockTest.java
+++ src/com/company/StaticBlockTest.java
@@ -5,5 +5,5 @@ public class StaticBlockTest {
     static {
         //可以简单理解为static在类中以顺序执行.
     }
-    
+
 }
